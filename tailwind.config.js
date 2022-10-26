9405dba8e2754168e9b64a10e2c0a536b72f7daf
Jay-Ubisse <joaquimubisse@gmail.com>
2022-10-26 23:05:21 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html"
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'bio': "url('../img/bio.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
