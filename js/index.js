57c234064bedc8e609a89d8c6d05618ef50df52c
Jay Ubisse <joaquimubisse@gmail.com>
2022-10-28 12:17:43 +0200

    applying animations to the projects section

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
     $('#about').hide();
     $('#skills').hide();
+
+    $('#projects-area').hide();
+    
+
     $(window).scroll(function() {
         if ($(this).scrollTop()<200) {
             $('#about').hide(500);
@@ -8,8 +12,15 @@ $(document).ready(function() {
          } else {
           $('#about').show(700);
          }
+
          if ($(this).scrollTop()>500) {
             $('#skills').show(1500);
+         }
+
+         if ($(this).scrollTop()>900) {
+            $('#projects-area').show(1200);
+         } else {
+            $('#projects-area').hide();
          }
      });
 });
